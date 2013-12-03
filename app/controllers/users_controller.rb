ac241724bd625d92c2c814e7c34a3abe309d841d
Marta Fonda <martafondapascual@gmail.com>
2013-12-03 11:23:11 +0100

    test GEM Rolify done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,13 @@ class UsersController < ApplicationController
     @user = User.friendly.find(params[:id])
     if @user.update(user_params)
       redirect_to @user
+    else
+      render action: 'edit'
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:admin)
+    params.require(:user).permit(:admin, :email)
   end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 describe UsersController do 
   let(:valid_session) { {} } 
   let(:user) { create(:user) } 
-  let(:invalid_user) { create(:user, street: nil) } 
   let(:valid_attributes) { attributes_for(:user) } 
 
   describe "GET index" do
@@ -16,4 +15,38 @@ describe UsersController do
       expect(assigns(:user)).to eq(user)
     end
   end
+  describe "GET edit" do
+    it "assigns an user to an @user" do
+      get :edit, { id: user.to_param }, valid_session
+      expect(assigns(:user)).to eq(user)
+    end
+  end
+  describe "PUT update" do
+    context "with valid attributes" do
+     it "updates the requested user" do 
+      a_user = create(:user, valid_attributes)
+      put :update, {
+        :id => a_user.to_param,
+        :user => {:email => "abc@mymail.com"}
+        }, valid_session
+    end
+
+    it "assigns the requested user as @user" do
+      put :update, {
+        :id => user.to_param,
+        :user => { :email => "abc@mymail.com" }
+        }, valid_session 
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it "redirects to the user page" do
+        put :update, {
+          id: user.to_param, 
+          :user => valid_attributes
+          }, valid_session
+        expect(response).to redirect_to(user)
+      end
+    end
+  end
+
 end
